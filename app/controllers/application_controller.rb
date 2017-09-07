0ec9c1fe9b5b59a1d6985163912e48a33e0ba233
taketoshitakeuchi <goodgolf722003@gmail.com>
2017-09-07 10:26:43 +0000

    sessionコントローラ生成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,15 @@
 class ApplicationController < ActionController::Base
 
-# 	require 'net/http'
-# 	require 'uri'
-# 	require 'json'
-  
-#   protect_from_forgery with: :exception
-  
-#   private
-#   def myprofile(myAccessToken)
-#   	url = "https://api.instagram.com/v1/users/3952372017/?access_token=#{myAccessToken}"
-  	
-# #   	url = "https://api.instagram.com/v1/users/3952372017/follows/access_token=#{myAccessToken}&count=3"
-  	
-#   	    url = "https://api.instagram.com/v1/users/self/requested-by?access_token=#{myAccessToken}&count=3&response_type=token&scope=public_content"
-  	
-# 		return JSON.parse(Net::HTTP.get(URI.parse(url)))
-#   end
-  
-#   def mylist(myAccessToken)
-#   	url = "https://api.instagram.com/v1/users/self/media/recent?access_token=#{myAccessToken}"
-# 		return JSON.parse(Net::HTTP.get(URI.parse(url)))
-#   end
-  
+	protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
   	
 end
